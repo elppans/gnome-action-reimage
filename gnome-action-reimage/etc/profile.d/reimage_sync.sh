8ea1d56b0f11707cbd2168ad25500b60cdf42b36
elppans <elppans@gmail.com>
2025-08-03 01:21:53 -0300

    Atualizado reimage_sync

--- gnome-action-reimage/etc/profile.d/reimage_sync.sh
+++ gnome-action-reimage/etc/profile.d/reimage_sync.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
 # Comando para sincronização do Action Script ReImage
-comando="$(basename $0)"
+comando="$(basename "$0")"
 
 # LOG
 LOG="/tmp/$comando.log"
 
 # Caminho de origem
-ORIGEM="/etc/skel/.local/share/nautilus/scripts/ReImage/"
+ORIGEM="/etc/skel/.local/share/nautilus/scripts/ReImage"
 # Caminho de destino
 DESTINO="$HOME/.local/share/nautilus/scripts/ReImage"
 
 # Cria o diretório de destino, se não existir
 mkdir -p "$DESTINO"
 
-# Sincroniza arquivos e diretórios ausentes do /etc/skel
-rsync -auv --ignore-existing "$ORIGEM" "$DESTINO" &>> "$LOG"
+# Renomeia diretórios e arquivos antes de sincronizar
+IDIOMAREI="reimage_$(echo "$LANG" | cut -d'.' -f1 | cut -d'_' -f1)"
+export IDIOMAREI
+
+
+
+if [ -d "$ORIGEM" ]; then
+    if command -v transname &> /dev/null; then
+        if [ ! -f "$ORIGEM/$IDIOMAREI" ]; then
+            transname "$ORIGEM"/*
+            transname "$ORIGEM"/*/*
+            touch "$ORIGEM/$IDIOMAREI"
+        fi
+    fi
+        # Sincroniza arquivos e diretórios "REImage" ausentes em $HOME com /etc/skel
+        rsync -auv --ignore-existing "$ORIGEM/" "$DESTINO" &>> "$LOG"
+fi
+
+
+
 
 
 
